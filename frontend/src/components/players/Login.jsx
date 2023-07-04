7fd92722f4543b29b0b1e0788dbd30a635eac272
Alok-jaiswal-075 <alokjaiswal075@gmail.com>
2023-07-04 21:58:53 +0530

    completed registration form

--- frontend/src/components/players/Login.jsx
+++ frontend/src/components/players/Login.jsx
@@ -71,34 +71,6 @@ const Login = () => {
 
         </div>
 
-
-
-        // <div className="container">
-        //     <div className="row">
-                // <form method="POST" className="row g-3 needs-validation" noValidate autoComplete="off">
-
-                //     <div className="col-12">
-                    // <label htmlFor="validationCustom02" className="form-label">Email</label>
-                    // <input type="email" className="form-control" id="validationCustom01" name="email" value={player.email} onChange={handleInput} required />
-                //     <div className="valid-feedback">
-                //         Looks good!
-                //     </div>
-                //     </div>
-
-                //     <div className="col-12">
-                //     <label htmlFor="validationCustom03" className="form-label">Password</label>
-                //     <input type="password" className="form-control" id="validationCustom02" name="password" value={player.password} onChange={handleInput} required />
-                //     <div className="valid-feedback">
-                //         Looks good!
-                //     </div>
-                //     </div>
-
-                //     <div className="col-12">
-                //     <button className="btn btn-success" type="submit" onClick={handleSubmit}>Login</button>
-                //     </div>
-                // </form>
-        //     </div>
-        // </div>
     )
 }
 
--- frontend/src/components/players/Register.jsx
+++ frontend/src/components/players/Register.jsx
@@ -6,7 +6,7 @@ const Register = () => {
 
     const navigate = useNavigate()
 
-    const [player, setPlayer] = useState({fname:"", lname:"", age:"", email:"", contact:"", password:""});
+    const [player, setPlayer] = useState({fname:"", lname:"", age:"", email:"", contact:"", password:"",cpassword:""});
 
     let field, value;
     const handleInput = (e) =>{
@@ -19,24 +19,31 @@ const Register = () => {
     const handleSubmit = async (e) =>{
         e.preventDefault();
 
-        const res = await fetch("/players", {
-            method: "POST",
-            headers: {
-                "Content-type" : "application/json"
-            },
-            body : JSON.stringify({
-                player : {fname : player.fname, lname:player.lname, age:player.age, email:player.email, contact:player.contact, password:player.password}
+        if(player.password === player.cpassword){
+            const res = await fetch("/players", {
+                method: "POST",
+                headers: {
+                    "Content-type" : "application/json"
+                },
+                body : JSON.stringify({
+                    player : {fname : player.fname, lname:player.lname, age:player.age, email:player.email, contact:player.contact, password:player.password}
+                })
             })
-        })
-
-        const data = await res.json()
-        if(!data){
-            window.alert("Invalid data")
+    
+            const data = await res.json()
+            if(!data){
+                window.alert("Invalid data")
+            }
+            else{
+                window.alert(data.msg)
+                navigate('/login')
+            }
         }
         else{
-            window.alert(data.msg)
-            navigate('/login')
+            window.alert('Password do not match!')
         }
+
+        
     }
 
     return(
@@ -48,12 +55,34 @@ const Register = () => {
 
                 <form method="POST" className="flex flex-col justify-center items-center gap-7" noValidate autoComplete="off">
 
+                    <input type="text" className="border-4 border-transparent border-b-col-bg-dark  max-w-full p-2 pl-10 text-lg focus:outline-none"  
+                    placeholder = "First Name" name="fname" value={player.fname} 
+                    onChange={handleInput} required />
+
+                    <input type="text" className="border-4 border-transparent border-b-col-bg-dark  max-w-full p-2 pl-10 text-lg focus:outline-none"  
+                    placeholder = "Last Name" name="lname" value={player.lname} 
+                    onChange={handleInput} required />
+
+                    <input type="number" className="border-4 border-transparent border-b-col-bg-dark  max-w-full p-2 pl-10 text-lg focus:outline-none"  
+                    placeholder = "Age" name="age" value={player.age} 
+                    onChange={handleInput} required />
+
                     <input type="email" className="border-4 border-transparent border-b-col-bg-dark  max-w-full p-2 pl-10 text-lg focus:outline-none"  
                     placeholder = "Email" name="email" value={player.email} 
                     onChange={handleInput} required />
 
+                    <input type="number" className="border-4 border-transparent border-b-col-bg-dark  max-w-full p-2 pl-10 text-lg focus:outline-none"  
+                    placeholder = "Contact" name="contact" value={player.contact} 
+                    onChange={handleInput} required />
+
+
                     <input type="password" className="border-4 border-transparent border-b-col-bg-dark  max-w-full p-2 pl-10 text-lg focus:outline-none"  
                     placeholder = "Password" name="password" value={player.password} 
+                    onChange={handleInput} required />
+
+
+                    <input type="text" className="border-4 border-transparent border-b-col-bg-dark  max-w-full p-2 pl-10 text-lg focus:outline-none"  
+                    placeholder = "Confirm Password" name="cpassword" value={player.cpassword} 
                     onChange={handleInput} required />
 
                     <button className="border border-col-btn bg-col-btn px-20 py-2 text-lg font-bold rounded-full hover:bg-transparent transition duration-300 ease-in-out" type="submit" onClick={handleSubmit}>Register</button>
@@ -65,6 +94,7 @@ const Register = () => {
             </div>
 
         </div>
+
     )
 }
 
