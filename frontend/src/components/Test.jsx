39c03435e629b18cf37a0921f088491793bfaa6b
Alok-jaiswal-075 <alokjaiswal075@gmail.com>
2023-07-04 17:53:28 +0530

    added colors and fonts

---
+++ frontend/src/components/Test.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Test = () => {
+    return(
+        <div className="bg-dark">
+            <div>this is test</div>
+        </div>
+    )
+}
+
+export default Test
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,19 @@ module.exports = {
   ],
   theme: {
     
-    extend: {},
+    extend: {
+      colors: {
+        'bg-primary': '#1A0E1C',
+        'bg-dark' : '#140B15',
+        'col-text' : '#B9B9B9', 
+        'link-inactive' : '#7C7C7C',
+        'col-btn' : '#F95333' 
+      },
+      fontFamily: {
+        'font-heading': ['"Hanuman"', 'serif'],
+        'font-text': ['"Poppins"', 'sans-serif']
+      },
+    },
   },
   plugins: [],
 }
