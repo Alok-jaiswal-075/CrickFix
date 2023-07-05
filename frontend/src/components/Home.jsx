a62bc5a44ae97ef43222033df05a8935c6acbc65
prabhatkr007 <pkr.876550@gmail.com>
2023-07-06 03:30:14 +0530

    home page added

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React,{useEffect, useState} from 'react'
 import Modal from './Modal'
+import backgroundImage from '../asset/Hero.png'; 
 const Home = () => {
 
     const [isOpen,setisopen] = useState(false);
@@ -30,14 +31,43 @@ const Home = () => {
       }, [])
     
       return(
-        <div className='bg-col-bg-primary'>
-          <h1 className="text-3xl  font-bold underline col-text font-heading">
-             check check !
-          </h1>
+        <>
+      
+        <div className='flex  items-center min-h-screen bg-cover' style={{ backgroundImage: `url(${backgroundImage})` }}>
 
-            <button className='btn btn-primary' onClick = {()=>setisopen(true)}>modal</button>
-            <Modal open={isOpen} onClose={() => setisopen(false)}>This is our modal</Modal>
+          <div className='pl-48 bg-transparent min-w-fit mb-42 absolute z-10'>
+
+            <p className='font-heading text-6xl leading-snug'>
+              Complete Match <br /> Organizing Facilities<br />
+              For Cricket Lovers <br /> like You
+            </p>
+
+            <p className='font-text text-xl leading-relaxed'>
+              CricFix provides you with features like Team Building, <br />
+              Competitions and Score Maintenance, so that you can enjoy <br /> 
+              your games to the fullest.
+            </p>
+
+            <button 
+            className='bg-col-btn mt-16 mr-10 pl-8 text-col-bg-primary pr-9 pt-4 pb-4 rounded text-xl ' > Join Now</button>
+
+            <button className=' text-col-btn border-col-btn rounded text-xl border pl-8 pr-9 pt-4 pb-4' >Know More</button>
+
+
+          </div>
+        
+
         </div>
+    
+          
+
+         <div>
+          
+            {/* <button className='btn btn-primary' onClick = {()=>setisopen(true)}>modal</button>
+            <Modal open={isOpen} onClose={() => setisopen(false)}>This is our modal</Modal> */}
+          </div>
+
+          </>
     )
 }
 
