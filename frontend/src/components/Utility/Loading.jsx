30fde521ef0965d8bca3dbe9dca13b29b7bbc021
Alok-jaiswal-075 <alokjaiswal075@gmail.com>
2023-07-11 20:05:46 +0530

    some changes to loader

--- frontend/src/components/Utility/Loading.jsx
+++ frontend/src/components/Utility/Loading.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import BounceLoader from "react-spinners/BounceLoader";
 import BarLoader from "react-spinners/BarLoader";
 import ClimbingBoxLoader from "react-spinners/ClimbingBoxLoader";
+import SquareLoader from 'react-spinners/SquareLoader'
+import HashLoader from 'react-spinners/HashLoader'
+import ScaleLoader from 'react-spinners/ScaleLoader'
+import SyncLoader from 'react-spinners/SyncLoader'
 
 function Loading() {
   return (
@@ -13,11 +17,13 @@ function Loading() {
       </div>
 
         {/* DIFFERENT lOADING ANIMATIONS TO TRY */}
-        <BarLoader size={150} color='#7C7C7C' />
-        {/* <img src={loaderPath} alt="Loader" /> */}
+        {/* <BarLoader size={150} color='#7C7C7C' /> */}
         {/* <BounceLoader color='#B9B9B9' /> */}
         {/* <SquareLoader color='#B9B9B9' /> */}
         {/* <ClimbingBoxLoader color='#B9B9B9' /> */}
+        {/* <HashLoader color='#B9B9B9' /> */}
+        {/* <ScaleLoader color='#F95333' /> */}
+        <SyncLoader color='#F95333' />
     </div>
   )
 }
