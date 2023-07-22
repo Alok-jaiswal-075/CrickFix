3025184db2ba078079e086f69da387df190a499d
Vaibhav <vaibhav.bajpai1221@gmail.com>
2023-07-23 00:19:40 +0530

    Fixed Edit Player page

--- frontend/src/components/players/EditPlayer.jsx
+++ frontend/src/components/players/EditPlayer.jsx
@@ -1,87 +1,71 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
+import { useNavigate, Link } from 'react-router-dom'
 
 const EditPlayer = () => {
+    const navigate = useNavigate()
 
-    const [player, setPlayer] = useState({fname:"", lname:"", age:"", oldpassword:"", newpassword:""});
+    const [player, setPlayer] = useState({ fname: "", lname: "", age: "", oldpassword: "", newpassword: "" });
 
     let field, value;
-    const handleInput = (e) =>{
+    const handleInput = (e) => {
         field = e.target.name;
         value = e.target.value;
 
-        setPlayer({...player,[field]:value})
+        setPlayer({ ...player, [field]: value })
     }
 
-    const handleSubmit = async (e) =>{
+    const handleSubmit = async (e) => {
         e.preventDefault();
 
         const res = await fetch("/players", {
             method: "PUT",
             headers: {
-                "Content-type" : "application/json"
+                "Content-type": "application/json"
             },
-            body : JSON.stringify({
-                player : {fname : player.fname, lname:player.lname, age:player.age, oldpassword:player.oldpassword, newpassword: player.newpassword}
+            body: JSON.stringify({
+                player: { fname: player.fname, lname: player.lname, age: player.age, oldpassword: player.oldpassword, newpassword: player.newpassword }
             })
         })
 
         const data = await res.json()
-        if(!data || res.status === 404){
+        if (!data || res.status === 404) {
             window.alert("Invalid data")
         }
-        else{
+        else {
             window.alert(data.msg)
+            navigate('/login')
             // console.log(data)
         }
     }
 
-    return(
-        <div className="container">
-            <div className="row">
-                <form className="row g-3 needs-validation" method="POST" noValidate autoComplete="off">
-                    <div className="col-12">
-                    <label htmlFor="validationCustom01" className="form-label">First name</label>
-                    <input type="text" className="form-control"  name="fname" value={player.fname} onChange={handleInput} required />
-                    <div className="valid-feedback">
-                        Looks good!
-                    </div>
-                    </div>
+    return (
+        <div className="mt-28 grid grid-cols-12 gap-4 sm:flex justify-center items-center">
+            <div className="bg-transparent flex flex-col justify-center items-center gap-3 col-start-5 col-span-4 font-text ">
+                <h1 className="my-7 sm:text-5xl text-2xl font-heading tracking-wider">Edit Player</h1>
+                <form className="flex flex-col justify-center items-center gap-7" method="POST" noValidate autoComplete="off">
+                    <input type="text" className="bg-transparent border-4 border-transparent border-b-col-bg-dark  max-w-full p-2 pl-10 sm:text-lg text-sm focus:outline-none"
+                        placeholder="First Name" name="fname" value={player.fname}
+                        onChange={handleInput} required />
 
-                    <div className="col-12">
-                    <label htmlFor="validationCustom02" className="form-label">Last name</label>
-                    <input type="text" className="form-control"  name="lname" value={player.lname} onChange={handleInput} required />
-                    <div className="valid-feedback">
-                        Looks good!
-                    </div>
-                    </div>
-                    
-                    <div className="col-12">
-                    <label htmlFor="validationCustom02" className="form-label">Age</label>
-                    <input type="number" className="form-control" name="age" value={player.age} onChange={handleInput} required />
-                    <div className="valid-feedback">
-                        Looks good!
-                    </div>
-                    </div>
+                    <input type="text" className="bg-transparent border-4 border-transparent border-b-col-bg-dark  max-w-full p-2 pl-10 sm:text-lg text-sm focus:outline-none"
+                        placeholder="Last Name" name="lname" value={player.lname}
+                        onChange={handleInput} required />
+
+                    <input type="number" className="bg-transparent border-4 border-transparent border-b-col-bg-dark  max-w-full p-2 pl-10 sm:text-lg text-sm focus:outline-none"
+                        placeholder="Age" name="age" value={player.age}
+                        onChange={handleInput} required />
 
 
-                    <div className="col-12">
-                    <label htmlFor="validationCustom02" className="form-label">Old Password</label>
-                    <input type="password" className="form-control" name="oldpassword" value={player.oldpassword} onChange={handleInput} required />
-                    <div className="valid-feedback">
-                        Looks good!
-                    </div>
-                    </div>
+                    <input type="password" className="bg-transparent border-4 border-transparent border-b-col-bg-dark  max-w-full p-2 pl-10 sm:text-lg text-sm focus:outline-none"
+                        placeholder="Old Password" name="oldpassword" value={player.oldpassword}
+                        onChange={handleInput} required />
+
+                    <input type="password" className="bg-transparent border-4 border-transparent border-b-col-bg-dark  max-w-full p-2 pl-10 sm:text-lg text-sm focus:outline-none"
+                        placeholder="New Password" name="newpassword" value={player.newpassword}
+                        onChange={handleInput} required />
 
                     <div className="col-12">
-                    <label htmlFor="validationCustom02" className="form-label">New Password</label>
-                    <input type="password" className="form-control" name="newpassword" value={player.newpassword} onChange={handleInput} required />
-                    <div className="valid-feedback">
-                        Looks good!
-                    </div>
-                    </div>
-
-                    <div className="col-12">
-                    <button className="btn btn-success" type="submit" onClick={handleSubmit}>Update Player</button>
+                        <button className="border border-col-btn bg-col-btn px-20 py-2 sm:text-lg text-sm font-bold rounded-full hover:bg-transparent transition duration-300 ease-in-out" type="submit" onClick={handleSubmit}>Update Player</button>
                     </div>
                 </form>
             </div>
--- frontend/src/components/players/Login.jsx
+++ frontend/src/components/players/Login.jsx
@@ -1,6 +1,5 @@
 import React,{useState} from "react";
 import {useNavigate,Link} from 'react-router-dom'
-import Button from "../Utility/Button";
 
 
 
