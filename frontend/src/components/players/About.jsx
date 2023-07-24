2a11e4066c3213507821c89bc23135467137132a
Vaibhav <vaibhav.bajpai1221@gmail.com>
2023-07-25 00:03:44 +0530

    Added joined teams and sorted the state in about component

--- frontend/src/components/players/About.jsx
+++ frontend/src/components/players/About.jsx
@@ -5,15 +5,24 @@ import Loading from "../Utility/Loading";
 
 const About = (props) => {
     let yourTeamsTemp = 1;
-    let joinedTeamsTemp = 1;
+    // let joinedTeamsTemp = 1;
     const navigate = useNavigate();
 
-    const [player, setPlayerData] = useState({})
-    const [playerCount, setPlayerCount] = useState(0)
-    const [loading, setLoading] = useState(true)
+    // const [player, setPlayerData] = useState({})
+    // const [playerCount, setPlayerCount] = useState(0)
+    // const [teamList, setTeamList] = useState([])
+    // const [loading, setLoading] = useState(true)
+
+    const [aboutState, setAboutState] = useState({
+        player: {},
+        playerCount: 0,
+        teamList: [],
+        loading: true
+    })
 
     const callPlayerPage = async () => {
         try {
+            // fetching player data
             const res = await fetch('/players', {
                 method: "GET",
                 headers: {
@@ -22,7 +31,10 @@ const About = (props) => {
                 },
                 credentials: "include"
             })
+            const playerData = await res.json();
+            // setPlayerData(data)
 
+            // fetching number of players
             const countRes = await fetch('/players/all-players', {
                 method: "GET",
                 headers: {
@@ -32,13 +44,40 @@ const About = (props) => {
                 credentials: "include"
             })
             const players = await countRes.json()
-            setPlayerCount(players.length)
+            // setPlayerCount(players.length)
 
-            const data = await res.json();
-            setPlayerData(data)
-            // console.log(player.captainOf)
-            // console.log(player.joined_teams)
-            setLoading(false)
+            // fetching all teams the player is in
+            const teamRes = await fetch('/teams',{
+                method : "GET",
+                headers : {
+                    "Accept" : "application/json",
+                    "Content-Type" : "application/json"
+                },
+                credentials : "include"
+            })
+
+            const teams = await teamRes.json()
+            let tempTeamList = []
+            playerData.captainOf.forEach((team)=>{
+                tempTeamList.push(team)
+            })
+            teams.forEach(team=>{
+                if(!tempTeamList.includes(team) && team.players.includes(playerData)){
+                    tempTeamList.push(team)
+                }
+            })
+            // setTeamList(tempTeamList)
+            
+            // console.log(aboutState.player.captainOf)
+            // console.log(aboutState.player.joined_teams)
+            // setLoading(false)
+
+            setAboutState({
+                player: playerData,
+                playerCount: players.length,
+                teamList: tempTeamList,
+                loading: false
+            })
 
         } catch (error) {
             window.alert(error.msg)
@@ -79,7 +118,7 @@ const About = (props) => {
     }
 
 
-    if (loading) {
+    if (aboutState.loading) {
         return (
             <Loading />
         )
@@ -87,7 +126,7 @@ const About = (props) => {
         return (
             <div className="m-4 flex flex-col items-center gap-20 sm:gap-24">
 
-                <h1 className="text-center text-4xl sm:text-5xl my-6">Hi, {player.fname}!</h1>
+                <h1 className="text-center text-4xl sm:text-5xl my-6">Hi, {aboutState.player.fname}!</h1>
 
                 {/* User Info Section */}
                 <div className="w-full flex flex-col items-center sm:flex-row sm:items-start sm:justify-center gap-16 sm:gap-10 ">
@@ -100,8 +139,8 @@ const About = (props) => {
                             {/* Find 'big-smile' in the img src and replace it with the names of different styles from below link to change avatar style */}
 
                             {/* https://www.dicebear.com/styles/bottts */}
-                            
-                            <img className="rounded-full w-1/2 sm:w-1/3" src={`https://api.dicebear.com/6.x/big-smile/svg?seed=${player.fname}&backgroundColor=b6e3f4,c0aede,d1d4f9&scale=90&accessoriesProbability=50&backgroundType=gradientLinear,solid`} alt="User" />
+
+                            <img className="rounded-full w-1/2 sm:w-1/3" src={`https://api.dicebear.com/6.x/big-smile/svg?seed=${aboutState.player.fname}&backgroundColor=b6e3f4,c0aede,d1d4f9&scale=90&accessoriesProbability=50&backgroundType=gradientLinear,solid`} alt="User" />
                         </div>
 
                         <div className="flex items-center justify-between gap-0">
@@ -113,24 +152,24 @@ const About = (props) => {
                         </div>
 
                         <ul className="list-none text-sm sm:text-base">
-                            <li className="my-2">Age: {player.age}</li>
-                            <li className="my-2">Email: {player.email}</li>
-                            <li className="my-2">Contact: {player.contact}</li>
+                            <li className="my-2">Age: {aboutState.player.age}</li>
+                            <li className="my-2">Email: {aboutState.player.email}</li>
+                            <li className="my-2">Contact: {aboutState.player.contact}</li>
                         </ul>
 
                         {/* <div className="card-body">
-                            <h5 className="card-title">{player.fname} {player.lname}</h5>
+                            <h5 className="card-title">{aboutState.player.fname} {aboutState.player.lname}</h5>
                         </div>
                         <ul className="list-group list-group-flush">
-                            <li className="list-group-item">Age: {player.age}</li>
-                            <li className="list-group-item">Email: {player.email}</li>
-                            <li className="list-group-item">Contact: {player.contact}</li>
-                            <li className="list-group-item">Ranking: {player.ranking}</li>
-                            <li className="list-group-item">Half Centuries: {player.centuries}</li>
-                            <li className="list-group-item">Centuries:  {player.centuries}</li>
-                            <li className="list-group-item">Total Score:  {player.total_score}</li>
-                            <li className="list-group-item">Highest Score: {player.highest_score}</li>
-                            <li className="list-group-item">Tournaments played: {player.tournaments_played}</li>
+                            <li className="list-group-item">Age: {aboutState.player.age}</li>
+                            <li className="list-group-item">Email: {aboutState.player.email}</li>
+                            <li className="list-group-item">Contact: {aboutState.player.contact}</li>
+                            <li className="list-group-item">Ranking: {aboutState.player.ranking}</li>
+                            <li className="list-group-item">Half Centuries: {aboutState.player.centuries}</li>
+                            <li className="list-group-item">Centuries:  {aboutState.player.centuries}</li>
+                            <li className="list-group-item">Total Score:  {aboutState.player.total_score}</li>
+                            <li className="list-group-item">Highest Score: {aboutState.player.highest_score}</li>
+                            <li className="list-group-item">Tournaments played: {aboutState.player.tournaments_played}</li>
                         </ul>
                         <div className="card-body">
                             <button type="button" className="btn btn-success btn-sm m-2" onClick={gotoEditPage}>Edit</button>
@@ -142,19 +181,22 @@ const About = (props) => {
 
                     {/* Player Stats Card */}
                     <div className="bg-col-bg-dark drop-shadow-lg inline-block w-full sm:w-2/5 p-4 px-8 rounded-xl relative">
-                        <h3 className="text-xl sm:text-3xl mb-4">Player Statistics</h3>
                         <div className="flex flex-row w-full items-center justify-between gap-5">
-                        <ul className="list-none text-sm sm:text-base">
-                            <li className="my-2">Half Centuries: {player.centuries}</li>
-                            <li className="my-2">Centuries:  {player.centuries}</li>
-                            <li className="my-2">Total Score:  {player.total_score}</li>
-                            <li className="my-2">Highest Score: {player.highest_score}</li>
-                            <li className="my-2">Tournaments played: {player.tournaments_played}</li>
-                        </ul>
-                        <div className="flex text-lg sm:text-2xl flex-col items-start justify-center gap-2 mr-4">
-                            <span>Rank :</span>
-                            <p><span className="text-6xl sm:text-8xl">{player.ranking}</span>/{playerCount}</p>
-                        </div>
+                            <div>
+                            <h3 className="text-xl sm:text-3xl mb-4">Player Statistics</h3>
+                                <ul className="list-none text-sm sm:text-base">
+                                    <li className="my-2">Half Centuries: {aboutState.player.centuries}</li>
+                                    <li className="my-2">Centuries:  {aboutState.player.centuries}</li>
+                                    <li className="my-2">Total Score:  {aboutState.player.total_score}</li>
+                                    <li className="my-2">Highest Score: {aboutState.player.highest_score}</li>
+                                    <li className="my-2">Tournaments played: {aboutState.player.tournaments_played}</li>
+                                </ul>
+                            </div>
+
+                            <div className="flex text-lg sm:text-2xl flex-col items-start justify-center gap-2 mr-4">
+                                <span>Rank :</span>
+                                <p><span className="text-6xl sm:text-8xl">{aboutState.player.ranking}</span>/{aboutState.playerCount}</p>
+                            </div>
                         </div>
                     </div>
 
@@ -162,20 +204,20 @@ const About = (props) => {
 
 
                 {/* Display teams with user as captain */}
-                <div className=" w-4/5 lg:w-2/3">
+                <div className=" w-full lg:w-2/3">
                     <h2 className="text-center text-3xl sm:text-4xl">Your Teams</h2>
-                    {player.captainOf && <div className="flex flex-row p-8 items-end gap-6 overflow-x-scroll w-[90%]">
-                        {player.captainOf.map((team) => <Team key={yourTeamsTemp++} team={team} isCaptain={1} />)}
+                    {aboutState.player.captainOf && <div className="flex flex-row p-8 items-end gap-6 overflow-x-scroll w-[90%]">
+                        {aboutState.player.captainOf.map((team) => <Team key={yourTeamsTemp++} team={team} isCaptain={1} />)}
                     </div>}
                 </div>
 
                 {/* Display teams with user in them */}
-                {/* <div className=" w-4/5 lg:w-2/3">
-                    <h2 className="text-center text-3xl sm:text-4xl">Your Teams</h2>
-                    <div className="flex flex-row p-8 items-end gap-6 overflow-x-scroll w-[90%]">
-                        {player.teams_joined.map((team) => <Team key={yourTeamsTemp++} team={team} isCaptain={0} />)}
-                    </div>
-                </div> */}
+                 <div className=" w-4/5 lg:w-2/3">
+                     <h2 className="text-center text-3xl sm:text-4xl">Joined Teams</h2>
+                     <div className="flex flex-row p-8 items-end gap-6 overflow-x-scroll w-[90%]">
+                         {aboutState.teamList.map((team) => <Team key={yourTeamsTemp++} team={team} isCaptain={0} />)}
+                     </div>
+                 </div>
             </div>
         )
     }
