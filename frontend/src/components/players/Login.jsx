27230caecd3ad730e3ff3ab41ad7bb86c973f112
Alok-jaiswal-075 <alokjaiswal075@gmail.com>
2023-08-08 20:59:23 +0530

    added credentials in login page

--- frontend/src/components/players/Login.jsx
+++ frontend/src/components/players/Login.jsx
@@ -28,7 +28,8 @@ const Login = () => {
             },
             body : JSON.stringify({
                 email,password
-            })
+            }),
+            credentials : 'include'
         })
 
         const data = await res.json()
